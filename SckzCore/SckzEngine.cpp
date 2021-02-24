f4d1d4c8612d3d397c92d9eb5a7d8d507b1a46fc
sckzor <sckzor@protonmail.com>
2021-02-24 18:37:21 -0500

    Created a 'Resources'folder to store all of the assets and shaders, makes the main directory nicer

--- SckzCore/SckzEngine.cpp
+++ SckzCore/SckzEngine.cpp
@@ -6,10 +6,10 @@ int main()
     win.CreateWindow("SckzEngine", 800, 600);
     sckz::Vulkan vkan;
     vkan.CreateVulkan(win);
-    vkan.CreatePipeline("vertex.spv", "fragment.spv");
-    sckz::Model m1 = vkan.CreateModel("flare.obj", "placeholder.png");
-    sckz::Model m2 = vkan.CreateModel("viking_room.obj", "placeholder.png");
-    sckz::Model m3 = vkan.CreateModel("untitled.obj", "placeholder.png");
+    vkan.CreatePipeline("Resources/vertex.spv", "Resources/fragment.spv");
+    sckz::Model m1 = vkan.CreateModel("Resources/flare.obj", "Resources/placeholder.png");
+    sckz::Model m2 = vkan.CreateModel("Resources/viking_room.obj", "Resources/placeholder.png");
+    sckz::Model m3 = vkan.CreateModel("Resources/untitled.obj", "Resources/placeholder.png");
 
     while (!win.QueryClose())
     {
