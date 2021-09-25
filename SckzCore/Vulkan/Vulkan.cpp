2a17e1f371834dd044023580edcb836f8eb74bd2
Charlie Sands <sckzor@protonmail.com>
2021-09-24 20:59:10 -0400

    added another meme

--- SckzCore/Vulkan/Vulkan.cpp
+++ SckzCore/Vulkan/Vulkan.cpp
@@ -662,6 +662,20 @@ namespace sckz
     // Also, yes I did make this ascii art all by myself, are you proud of me? //
     /////////////////////////////////////////////////////////////////////////////
 
+    /////////////////////////////////////////////////////////////////////////////
+    //  For McElmo21             ___                                           //
+    // ____________             /o o\  \   /                     ____________  //
+    // \          |             \_o_/   \ /                      \          |  //
+    // | (x)  (x) / -   - ===|=  /|\     |   =|=== -       -     | (x)  (x) /  //
+    // /    ^     |          ||-/ | \  \ | /-||                  /    ^     |  //
+    // |  /\/\/\  \               |     \|/  Okay, SpingSlayer69 |  /\/\/\  \  //
+    // |__________|              / \   / o \     be that way,    |__________|  //
+    //   |      |               /   \  \___/ defend Skelegorg's    |      |    //
+    // xx|    xx|                                  humor...      xx|    xx|    //
+    //                                       ur name is dumb anyway            //
+    //                                                                         //
+    /////////////////////////////////////////////////////////////////////////////
+
     void Vulkan::CreateSyncObjects()
     {
         imageAvailableSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
